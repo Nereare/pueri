8da8581cd23ba58509c8e5231de1d64eef16eaa9
Igor Padoim <igorpadoim@gmail.com>
2019-10-19 18:45:20 -0300

    Add dependencies

--- lib/pueri.rb
+++ lib/pueri.rb
@@ -2,6 +2,9 @@
 
 require 'pueri/version'
 
+require 'pastel'
+require 'tty-table'
+
 # This module is a support for physicians and nurses who work on Primary Care
 # with children age 12 and younger. It calculates the child's age and outputs
 # an optimal vaccination scheme for such age, based on Brazil's National Program
